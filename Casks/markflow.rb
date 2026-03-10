b4200703210d19d1dfed3bd774ff6ed19c3bb952
mostlyfine <mostlyfine@gmail.com>
2026-03-10 22:34:40 +0900

    feat: add markflow cask formula

---
+++ Casks/markflow.rb
@@ -0,0 +1,24 @@
+cask "markflow" do
+  version "0.1.0"
+  sha256 "dc579d362caa444030bca4db7157a1faa5a3d8402c86b73b32ac4e31b0e9b6ef"
+
+  url "https://github.com/mostlyfine/markflow/releases/download/v#{version}/MarkFlow.dmg"
+  name "MarkFlow"
+  desc "Markdown viewer for macOS"
+  homepage "https://github.com/mostlyfine/markflow"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :sonoma"
+
+  app "MarkFlow.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.example.markflow",
+    "~/Library/Preferences/com.example.markflow.plist",
+    "~/Library/Caches/com.example.markflow",
+  ]
+end
